cccb6e8e430970d03997c8de2b812c43b547dbd4
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-08 11:40:08 +0300

    Installed package pillow

--- news/urls.py
+++ news/urls.py
@@ -6,6 +6,6 @@ urlpatterns=[
     url('^$',views.news_of_day,name="theNews"),
     # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name="pastNews"),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
-    url(r'^search/',views.search_results,name="search_results")
+    url(r'^',views.search_results,name="search_results")
 
 ]
