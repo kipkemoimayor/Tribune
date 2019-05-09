63a4e5ce6692fc58c8fb6f52fe9ddb3b7d3045ac
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-09 10:12:01 +0300

    deployment

--- news/models.py
+++ news/models.py
@@ -40,7 +40,7 @@ class Article(models.Model):
     editor=models.ForeignKey(Editor)
     tags=models.ManyToManyField(tags)
     pub_date=models.DateTimeField(auto_now_add=True)
-    article_image=models.ImageField(upload_to='articles/')
+    article_image=models.ImageField(upload_to='articles/',blank=True)
 
 
     @classmethod
--- tribune/settings.py
+++ tribune/settings.py
@@ -11,11 +11,18 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
+
+MODE=config('MODE',default="dev")
+SECRET_KEY=config('SECRET_KEY')
+DEBUG=config("DEBUG",default=False,cast=bool)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -42,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,16 +84,29 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'tribune',
-        'USER':'collo',
-        'PASSWORD':'collins '
+if config('MODE')=='dev':
+
+
+    DATABASES = {
+        'default': {
+            # 'ENGINE': 'django.db.backends.sqlite3',
+            # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE':'django.db.backends.postgresql',
+            'NAME':'tribune',
+            'USER':'collo',
+            'PASSWORD':'collins '
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+        default=config("DATABASE_URL")
+        )
+    }
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS=config("ALLOWED_HOSTS",cast=Csv())
 
 
 # Password validation
@@ -123,10 +144,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+django_heroku.settings(locals())
